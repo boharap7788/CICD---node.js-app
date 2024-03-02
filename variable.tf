951c4027f20dba8503b126a2f280bdadb4165513
Prakash Bohara <142651511+boharap7788@users.noreply.github.com>
2024-03-02 14:54:05 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,12 +1,10 @@
 variable "access_key" {
  description="access key !"
     type=string
-  default = "AKIATSV6HYKCU452KJGA"
 }
 variable "secret_key" {
  description="This is secret key !!!"
     type=string
-  default = "W+mOD7C+EmVgQK833IkQr4dTYDobfZejriwm9Qcl"
 }
 variable "region" {
   description="This is region"
