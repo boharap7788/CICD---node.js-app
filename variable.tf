81ba20a0055351e339239befa01947b8443e6b01
Prakash Bohara <142651511+boharap7788@users.noreply.github.com>
2024-03-02 14:50:20 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "access_key" {
+ description="access key !"
+    type=string
+  default = "AKIATSV6HYKC5YG4UA6R"
+}
+variable "secret_key" {
+ description="This is secret key !!!"
+    type=string
+  default = "OVpPz7DDdjFj7/vroK9HxOT0A5Qkh+0wQej7AsNj"
+}
+variable "region" {
+  description="This is region"
+    type=string
+  default = "ap-south-1"
+}
